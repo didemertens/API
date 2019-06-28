c8624b9791223607670aec8ffd4cf06bfd8088b6
Dide Mertens <d.g.j.mertens@gmail.com>
2019-06-28 21:30:16 +0100

    added swagger-docs

--- blogapi/blog_project/settings.py
+++ blogapi/blog_project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     # 3rd party
     'rest_framework',
     'rest_framework.authtoken',
+    'rest_framework_swagger',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
@@ -144,3 +145,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 SITE_ID = 1
 
+SWAGGER_SETTINGS = {
+  'LOGIN_URL' : 'rest_framework:login',
+  'LOGOUT_URL' : 'rest_framework:logout'
+}
--- blogapi/blog_project/urls.py
+++ blogapi/blog_project/urls.py
@@ -17,10 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.documentation import include_docs_urls
 from rest_framework.schemas import get_schema_view
+from rest_framework_swagger.views import get_swagger_view
 
 API_TITLE = 'Blog API'
 API_DESCRIPTION = 'A Web API for creating and editing blog posts.'
-schema_view = get_schema_view(title=API_TITLE)
+schema_view = get_swagger_view(title=API_TITLE)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -31,4 +32,5 @@ urlpatterns = [
     path('docs/', include_docs_urls(title = API_TITLE,
                                     description=API_DESCRIPTION)),
     path('schema/', schema_view),
+    path('swagger-docs', schema_view),
 ]
